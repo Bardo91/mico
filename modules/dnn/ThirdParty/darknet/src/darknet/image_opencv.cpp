080de6dd1353b45a6d0a8baf8ff27f19487866da
Bardo91 <19Bardo91@gmail.com>
2019-12-10 06:35:49 +0100

    Fixed include in darknet

--- modules/dnn/ThirdParty/darknet/src/darknet/image_opencv.cpp
+++ modules/dnn/ThirdParty/darknet/src/darknet/image_opencv.cpp
@@ -7,7 +7,7 @@
 #include "opencv2/highgui/highgui_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/core/version.hpp"
-#include "opencv2/videoio/legacy/constants_c.h"
+//#include "opencv2/videoio/legacy/constants_c.h"
 
 #include "opencv2/videoio/videoio_c.h"
 
